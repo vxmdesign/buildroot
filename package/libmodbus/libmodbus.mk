9330c6a04175d6de5755ac04b99fee25fd0f9fdf
vxmdesign <cjwfirmware@gmail.com>
2014-07-20 01:19:07 -0400

    Twinleaf modbus fix

--- package/libmodbus/libmodbus.mk
+++ package/libmodbus/libmodbus.mk
@@ -4,10 +4,12 @@
 #
 ################################################################################
 
-LIBMODBUS_VERSION = 3.0.5
-LIBMODBUS_SITE = http://libmodbus.org/site_media/build/
+LIBMODBUS_VERSION = master
+LIBMODBUS_SOURCE = master.tar.gz
+LIBMODBUS_SITE = https://github.com/twinleaf/libmodbus/archive
 LIBMODBUS_LICENSE = LGPLv2.1+
 LIBMODBUS_LICENSE_FILES = COPYING.LESSER
 LIBMODBUS_INSTALL_STAGING = YES
+LIBMODBUS_AUTORECONF = YES
 
 $(eval $(autotools-package))
